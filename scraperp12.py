4eced547d48790f4c5a4db6782dabd6c2a0fa7bf
Robin Romero <ingrobinromero@gmail.com>
2021-01-23 00:34:39 -0500

    agregando funcion para parseo de secciones, y obteniendo los links de cada noticia en la seccion dada

--- scraperp12.py
+++ scraperp12.py
@@ -1,29 +1,44 @@
-from lxml.html import parse
 import requests
 from bs4 import BeautifulSoup
 
 URL='https://www.pagina12.com.ar/'
 
 
+def parse_sections(link_section):
+    try:
+        response_link_section=requests.get(link_section)
+        if response_link_section.status_code==200:
+            html_section=BeautifulSoup(response_link_section.text, 'lxml')
+            articles=html_section.find_all('div', attrs={'class': 'article-item__content'})
+            # print(articles)
 
+            links_to_news=[article.a.get('href') for article in articles]
+            return links_to_news
+        else:
+            raise ValueError(f'Error: {response_link_section.status_code}')
 
-def parse_home(url):
+    except ValueError as ve:
+        print(ve)
+
+def parse_home_p12(url):
     try:
         response_p12=requests.get(url)
         if response_p12.status_code==200:
             html_home=BeautifulSoup(response_p12.text, 'lxml')
             # print(html_home.prettify())
-            
-            secciones=html_home.find('div', attrs={'class':'p12-dropdown-content'})
-            print(secciones.prettify())
-            # .find_all('a', attrs={'class':'p12-dropdown-item'})
+
+            sections=html_home.find('div', attrs={'class':'p12-dropdown-content'}).find_all('a', attrs={'class':'p12-dropdown-item'})
+            links_to_sections =[section.get('href') for section in sections]
+            return links_to_sections
+            # print(links_to_sections)
         else:
             raise ValueError(f'Error: {response_p12.status_code} ')
     except ValueError as ve:
         print('ve')
 
 def run():
-    parse_home(URL)
-
+    links_to_sections=parse_home_p12(URL)
+    links_news=parse_sections(links_to_sections[0])
+    print(links_news)
 if __name__ == '__main__':
     run()
