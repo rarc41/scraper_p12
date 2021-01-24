a59952de064131c36f28a031214500c8bada891b
Robin Romero <ingrobinromero@gmail.com>
2021-01-23 21:43:05 -0500

    modifiacion de las funciones, ahora cuantan con los return statement, para modularizar el codigo

--- scraperp12.py
+++ scraperp12.py
@@ -4,57 +4,58 @@ from bs4 import BeautifulSoup
 URL='https://www.pagina12.com.ar/'
 
 
-def parse_notices(link_notice): #esta funcion extrae titulo, volanta, copete, contenido y autor de la noticia
+def parse_notice(link_notice): #esta funcion extrae titulo, volanta, copete, contenido y autor de la noticia
+    #diccionaro para guardar informacion de la noticia
+    info_notice={}
     try:
         note=requests.get(link_notice)
         if note.status_code==200:
             html_note=BeautifulSoup(note.text, 'lxml')
+
             #extraemos el titulo
-            title=html_note.find('h1', attrs={'class':'article-title'}).text
-            print(title)
+            title=html_note.find('h1', attrs={'class':'article-title'})
+            if title:
+                info_notice['Titulo']=title.text
+            else:
+                info_notice['Titulo']=None
 
             #extraemos la fecha del articulo
-            fecha=html_note.find('span', attrs={'pubdate':'pubdate'}).get('datetime')
-            print(fecha)
+            fecha=html_note.find('span', attrs={'pubdate':'pubdate'})
+            if fecha:
+                info_notice['Fecha_publicacion']=fecha.get('datetime')
+            else:
+                info_notice['Fecha_publicacion']=None
 
             #si la noticia incluye una volanta, la extraemos
-            try:
-                volanta=html_note.find('h2', attrs={'class':'article-prefix'}).text
-                if volanta:
-                    print(volanta)
-                else:
-                    raise Exception(f'error: esta nota no tiene una volanta')
-            except Exception as e:
-                print(e)
+            volanta=html_note.find('h2', attrs={'class':'article-prefix'})
+            if volanta:
+                info_notice['Volanta']=volanta.text
+            else:
+                info_notice['Volanta']=None
 
             #si la noticia incluye un copete, lo extraemos
-            try:
-                copete=html_note.find('div', attrs={'class':'article-summary'}).text
-                if copete:
-                    print(copete)
-                else:
-                    raise Exception(f'error: esta nota no tiene una volanta')
-            except Exception as e:
-                print(e)
+            copete=html_note.find('div', attrs={'class':'article-summary'})
+            if copete:
+                info_notice['Copete']=copete.text
+            else:
+                info_notice['Copete']=None
 
-            body=html_note.find('div', attrs={'class':'article-text'}).get_text(' ')
-            print(body)
+            body=html_note.find('div', attrs={'class':'article-text'})
+            info_notice['Body']=body.get_text(' ')
 
             #extraemos el author, si es que lo tiene
-            try:
-                author=html_note.find('div', attrs={'class':'article-author'}).a.text
-                print(author)
-            except Exception as e:
-                print(e)
-                print('\n')
-
-        else:
-            raise Exception(f'{note.status_code}')
+            author=html_note.find('div', attrs={'class':'article-author'})
+            if author:
+                info_notice['Author']=author.a.text
+            else:
+                info_notice['Author']=None
 
     except Exception as e:
         print('Error: ')
         print(e)
         print('\n')
+
+    return info_notice
 
 def parse_sections(link_section):
     try:
@@ -62,18 +63,16 @@ def parse_sections(link_section):
         if response_link_section.status_code==200:
             html_section=BeautifulSoup(response_link_section.text, 'lxml')
             articles=html_section.find_all('div', attrs={'class': 'article-item__content'})
-            # print(articles)
 
             links_to_news=[article.a.get('href') for article in articles]
-            # return links_to_news
-            print(links_to_news)
+            return links_to_news
 
-            parse_notices(links_to_news[0])
         else:
             raise ValueError(f'Error en la request, status_code: {response_link_section.status_code}')
 
     except ValueError as ve:
         print(ve)
+
 
 def parse_home_p12(url):
     try:
@@ -84,10 +83,7 @@ def parse_home_p12(url):
 
             sections=html_home.find('div', attrs={'class':'p12-dropdown-content'}).find_all('a', attrs={'class':'p12-dropdown-item'})
             links_to_sections =[section.get('href') for section in sections]
-            # return links_to_sections
-            # print(links_to_sections)
-
-            parse_sections(links_to_sections[0])
+            return links_to_sections
 
         else:
             raise ValueError(f'Error: {response_p12.status_code} ')
@@ -95,6 +91,11 @@ def parse_home_p12(url):
         print('ve')
 
 def run():
-    parse_home_p12(URL)
+    links_to_sections=parse_home_p12(URL)
+    # print(links_to_sections)
+    links_to_notices=parse_sections(links_to_sections[0])
+    # print(links_to_notices)
+    print(parse_notice(links_to_notices[2]))
+    
 if __name__ == '__main__':
     run()
