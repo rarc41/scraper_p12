7ecd24de463f202d4ca2653421ee3d36816afea4
Robin Romero <ingrobinromero@gmail.com>
2021-01-23 19:14:11 -0500

    agregando funcion para la extracion de titulo, author, fecha, volanta, copete y contenido de las nmoticias

--- scraperp12.py
+++ scraperp12.py
@@ -4,6 +4,58 @@ from bs4 import BeautifulSoup
 URL='https://www.pagina12.com.ar/'
 
 
+def parse_notices(link_notice): #esta funcion extrae titulo, volanta, copete, contenido y autor de la noticia
+    try:
+        note=requests.get(link_notice)
+        if note.status_code==200:
+            html_note=BeautifulSoup(note.text, 'lxml')
+            #extraemos el titulo
+            title=html_note.find('h1', attrs={'class':'article-title'}).text
+            print(title)
+
+            #extraemos la fecha del articulo
+            fecha=html_note.find('span', attrs={'pubdate':'pubdate'}).get('datetime')
+            print(fecha)
+
+            #si la noticia incluye una volanta, la extraemos
+            try:
+                volanta=html_note.find('h2', attrs={'class':'article-prefix'}).text
+                if volanta:
+                    print(volanta)
+                else:
+                    raise Exception(f'error: esta nota no tiene una volanta')
+            except Exception as e:
+                print(e)
+
+            #si la noticia incluye un copete, lo extraemos
+            try:
+                copete=html_note.find('div', attrs={'class':'article-summary'}).text
+                if copete:
+                    print(copete)
+                else:
+                    raise Exception(f'error: esta nota no tiene una volanta')
+            except Exception as e:
+                print(e)
+
+            body=html_note.find('div', attrs={'class':'article-text'}).get_text(' ')
+            print(body)
+
+            #extraemos el author, si es que lo tiene
+            try:
+                author=html_note.find('div', attrs={'class':'article-author'}).a.text
+                print(author)
+            except Exception as e:
+                print(e)
+                print('\n')
+
+        else:
+            raise Exception(f'{note.status_code}')
+
+    except Exception as e:
+        print('Error: ')
+        print(e)
+        print('\n')
+
 def parse_sections(link_section):
     try:
         response_link_section=requests.get(link_section)
@@ -15,6 +67,8 @@ def parse_sections(link_section):
             links_to_news=[article.a.get('href') for article in articles]
             # return links_to_news
             print(links_to_news)
+
+            parse_notices(links_to_news[0])
         else:
             raise ValueError(f'Error en la request, status_code: {response_link_section.status_code}')
 
